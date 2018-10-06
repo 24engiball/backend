180d15afe0d04df402a8df87fc57b967b06b8ac5
24engiball <engiball@gmail.com>
2018-10-07 00:29:05 +0700

    add file

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,6 @@
 FROM richarvey/nginx-php-fpm:latest
 ENV WEBROOT /var/www/html/public
 COPY  . .
+RUN chmod 755 setup_database.sh
 
 #docker image build -t api .
---
+++ setup_database.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+while !(mysqladmin ping --host=$DB_HOST -utask -psecret)
+do
+    sleep 1
+    echo "Wait for mySQL ..."
+done
+echo "MySql ready"
+
+ php artisan key:generate
+ php artisan migrate:refresh --seed --force
+ php artisan migrate --seed --force
