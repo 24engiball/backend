86febd314780e14bbe876dc8db79d4d3f82dcaea
24engiball <engiball@gmail.com>
2018-10-07 21:49:50 +0700

    commit

--- build_image.sh
+++ build_image.sh
@@ -3,4 +3,4 @@ docker rmi taskapi:0.0.2
 docker container run --rm -v $(pwd):/src -w /src composer update
 docker image build -t taskapi:0.0.2 .
 docker image tag taskapi:latest engiball/taskapi:0.0.2
-docker push engiball/taskapi:0.0.1
+docker push engiball/taskapi:0.0.2
